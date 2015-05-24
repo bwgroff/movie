6e0b1011d4270c77e600231bcf6180c0f2c5736c
Brad Groff <bradleywgroff@gmail.com>
2015-05-24 14:57:33 +0000

    this version is broken too but oh well - will have to fix

--- es_ingest.py
+++ es_ingest.py
@@ -1,3 +1,4 @@
+import time
 import requests
 from datetime import datetime
 import multiprocessing
@@ -12,34 +13,15 @@ def insert_results(data, index_name):
     for cocaine_mention in data["items"]:
         _date = cocaine_mention["date"]
         cocaine_mention["date"] = datetime(int(_date[:4]), int(_date[4:6]), int(_date[6:]))
-        #print "    insert"
         es.index(index=index_name, body=cocaine_mention, doc_type="article")
 
-fake_data = {"items":[{"date":"20120101","body":"hey!"}]}
-#insert_results(fake_data, "test")
 
-#print "index"
-#print es.index(index="test", doc_type="test_doc", id=1, body=fake_data)
-#print "get"
-#print es.get(index="test", doc_type="test_doc", id=1)
-#print "search full date"
-#print es.search(index="test", q = 'date:"20120101"')#body={"query": {"match_all": {}}})
-#print "search partial date"
-#print es.search(index="test", q = 'date:"2012"')#body={"query": {"match_all": {}}})
-##print "cocaine search"
-##print es.search(index="cocaine", q = 'the')#body={"query": {"match_all": {}}})
-search_term = raw_input("Enter search term to build database: ")
-get_background = raw_input("Fetch background data? [y/n] ") in ["y", "Y", "yes", "Yes"]
-term_url = "http://chroniclingamerica.loc.gov/search/pages/results/?date1={year}&date2={year}&dateFilterType=yearRange&andtext={term}&format=json&page=".format(term=search_term)
+term_url = "http://chroniclingamerica.loc.gov/search/pages/results/?date1={year}&date2={year}&dateFilterType=yearRange&andtext={term}&format=json&page="
 bg_url = "http://chroniclingamerica.loc.gov/search/pages/results/?date1={year}&date2={year}&dateFilterType=yearRange&format=json&page="
 
-#print "request"
-#r_data = requests.get(cocaine_url.format(year="1900")).json()
-#insert_results(r_data, "r_test")
-#print es.search(index="r_test")
 
-def download_firstn_results(n, initial_page=1, base_url=term_url, index_name=search_term):
-    n = min(n, 56145)
+def download_firstn_results(max_n, initial_page=1, base_url=term_url, index_name=search_term):
+    n = min(max_n, 56145)
     this_page = initial_page
     end_index = 0
     while end_index < n:
@@ -50,34 +32,44 @@ def download_firstn_results(n, initial_page=1, base_url=term_url, index_name=sea
             logging.debug("Error in page " + str(this_page))
         this_page += 1
         end_index = response["endIndex"]
+        n = min(response["totalItems"], max_n)
+
+available_years = range(1885,1920)
 
 
-# download_firstn_results(99999999, base_url=cocaine_url, db=cocaine_db) # careful running this, will take forever.
-
-available_years = range(1836,1923)
-
-
-def func(y, url, index_name):
-    print "fetching year {year}".format(year=y)
-    download_firstn_results(1000, base_url=url.format(year=y), index_name=index_name)
+def func(y, url, index_name, count = 56145):
+    print "fetching data from {year} for index {index_name}.".format(year=y, index_name=index_name)
+    download_firstn_results(count, base_url=url, index_name=index_name)
 
 def term_func(y):
-    func(y, term_url, search_term)
+    func(y, term_url.format(year=y, term=search_term), search_term)
 
 def bg_func(y):
-    func(y, date_url, "bg")
+    func(y, bg_url.format(year=y), "background")
 
 def multi_fetch(f_type, index_name):
     if f_type == "specific":
-        f = cocaine_func
+        f = term_func
     else:
         f = bg_func
     p = multiprocessing.Pool(5)  # increase if needed, beware diminishing returns
     p.map(f, available_years)
 
-print "cocaine data fetching"
-multi_fetch("specific", search_term)
+#t0 = time.time()
+
+
+get_term = raw_input("Fetch new term data? [y/n] ") in ["y", "Y", "yes", "Yes"]
+if get_term: 
+    search_term = raw_input("Enter search term to build database: ")
+get_background = raw_input("Fetch background data? [y/n] ") in ["y", "Y", "yes", "Yes"]
+if get_term:
+    print "term data fetching"
+    multi_fetch("specific", search_term)
+#    t1 = time.time()
+#    print "Time elapsed while building term-specific database: {time}".format(time=t1-t0)
 if get_background:
+    base_count = 1000
     print "background data fetching"
     multi_fetch("general", "background")
-
+#    t2 = time.time()
+#    print "Time elapsed while building background database: {time}".format(time=t2-t1)
